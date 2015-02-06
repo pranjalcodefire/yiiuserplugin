67bcbd493434708e2553a2f53f40b5e28aa9e707
Ankit <ankit@codefire.in>
2015-02-06 12:02:16 +0530

    new changes

--- common/components/MyGlobalClass.php
+++ common/components/MyGlobalClass.php
@@ -1,10 +1,17 @@
 <?php 
 namespace common\components;
+use Yii;
+include_once '/../config/constants.php';
 
 class MyGlobalClass extends \yii\base\Component{
     public function init() {
-        echo "<h3>Hi</h3>";
-        parent::init();
+		parent::init();
+		//echo '<pre>'; print_r(Yii::$app); exit;
+		// if (Yii::$app->user->can('backend:User:Index')) {
+			// echo 'hi';
+		// }else{
+			// echo 'hello';
+		// }
     }
 }
 ?>
